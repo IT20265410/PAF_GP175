870b30d14b4400d2eb853454198e066bf87152c9
thuli99 <thulyapremathilaka@gmail.com>
2022-04-22 15:56:12 +0530

    customer update method
    service and user java class

--- src/com/UserService.java
+++ src/com/UserService.java
@@ -7,6 +7,9 @@ import model.User;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 /*//For JSON
 import com.google.gson.*;
 
@@ -19,6 +22,16 @@ import org.jsoup.nodes.Document;
 public class UserService {
 	
 	User userObj = new User();
+	
+	//view customer details
+	/*
+	 * @GET
+	 * 
+	 * @Path("/{userId}")
+	 * 
+	 * @Produces(MediaType.TEXT_HTML) public String readCustomer() { return
+	 * userObj.readCustomer(); }
+	 */
 	
 	//Add customer (registration)
 	@POST
@@ -39,6 +52,29 @@ public class UserService {
 		// pass these values to the register() method, return into a String variable
 		// named output, which can be returned to the client as the response.
 		String output = userObj.register(firstName, lastName, NIC, address, phone, email);
+		return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String customerData) {
+		
+		// Convert the input string to a JSON object
+		JsonObject UserObject = new JsonParser().parse(customerData).getAsJsonObject();
+
+		// Read the values from the JSON object
+		String userId = UserObject.get("userId").getAsString();
+		String firstName = UserObject.get("firstName").getAsString();
+		String lastName = UserObject.get("lastName").getAsString();
+		String NIC = UserObject.get("NIC").getAsString();
+		String address = UserObject.get("address").getAsString();
+		String phone = UserObject.get("phone").getAsString();
+		String email = UserObject.get("email").getAsString();
+
+
+		String output = userObj.updateCustomer(userId, firstName, lastName, NIC, address, phone, email);
 		return output;
 	}
 }
