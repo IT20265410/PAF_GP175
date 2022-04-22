2bb80d526339102326af73d811c3d95574e12e38
thuli99 <thulyapremathilaka@gmail.com>
2022-04-22 13:11:34 +0530

    User insert method
    UserService, User java classes

--- src/com/UserService.java
+++ src/com/UserService.java
@@ -1,5 +1,45 @@
+//To implement the RESTful API
 package com;
 
-public class UserService {
+import model.User;
 
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/*//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+*/
+@Path("/Users")
+public class UserService {
+	
+	User userObj = new User();
+	
+	@POST
+	@Path("/")
+	// To specify the input type as form data
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+
+	// To specify the form elements as the parameters to the register() method
+	public String register(@FormParam("firstName") String firstName, 
+							@FormParam("lastName") String lastName,
+							@FormParam("NIC") String NIC, 
+							@FormParam("homeNo") String homeNo,
+							@FormParam("street") String street,
+							@FormParam("city") String city,
+							@FormParam("phone") int phone,
+							@FormParam("email") String email)
+	
+	{
+		// pass these values to the register() method, return into a String variable
+		// named output, which can be returned to the client as the response.
+		String output = userObj.register(firstName, lastName, NIC, homeNo,street,city,phone,email);
+		return output;
+	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -5,20 +5,54 @@ import java.sql.*;
 import java.sql.Connection;
 
 public class User {
-	
+
 	// A common method to connect to the DB
-		private Connection connect() {
-			Connection con = null;
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
 
-				// Provide the correct details: DBServer/DBName, username, password
-				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "thulya123");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return con;
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "thulya123");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		return con;
+	}
 
+	public String register(String fname, String lname, String nic, String homeno, String street, String city, int phone, String email) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			
+			// create a prepared statement
+			String query = " insert into user (`userId`,`firstName`,`lastName`,`NIC`,`homeNo`, `street`, `city`, `phone`, `email`)"
+					+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, fname);
+			preparedStmt.setString(3, lname);
+			preparedStmt.setString(4, nic);
+			preparedStmt.setString(5, homeno);
+			preparedStmt.setString(6, street);
+			preparedStmt.setString(7, city);
+			preparedStmt.setInt(8, phone);
+			preparedStmt.setString(9, email);
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "User registered successfully";
+		} catch (Exception e) {
+			output = "Error while registering the user.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 
 }
