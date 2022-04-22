db85c9856d2b4e20e9974661e60d951a479bfe6e
thuli99 <thulyapremathilaka@gmail.com>
2022-04-22 12:17:15 +0530

    DB connection

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,24 @@
+//To implement the server-model 
 package model;
 
+import java.sql.*;
+import java.sql.Connection;
+
 public class User {
+	
+	// A common method to connect to the DB
+		private Connection connect() {
+			Connection con = null;
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+
+				// Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "thulya123");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return con;
+		}
+
 
 }
