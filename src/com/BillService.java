75a8a95a55fad1bbd7899302d81baa34a31a69fb
DGNLW522 <naveenlakshan522@gmail.com>
2022-04-24 20:00:36 +0530

    addBill method

--- src/com/BillService.java
+++ src/com/BillService.java
@@ -1,5 +1,99 @@
+//To implement the RESTful API
 package com;
 
+import model.Bill; 
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For XML
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+//For JSON
+import com.google.gson.*;
+
+@Path("/Bills")
 public class BillService {
+
+	Bill billObj = new Bill();
+
+	//view a particular customer details
+	@GET
+	@Path("/{billId}")
+	@Produces(MediaType.TEXT_HTML)
+	public String viewBill(@PathParam("billId") String billId) {
+		return billObj.viewBill(billId); 
+	}
+
+	//Add customer(registration)
+	@POST
+	@Path("/")
+	// To specify the input type as form data
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+
+	// To specify the form elements as the parameters to the register() method
+	public String addBill(@FormParam("issueDate") String issueDate, @FormParam("unit") int unit,
+			@FormParam("balance") Float balance, @FormParam("amountToPay") Float amountToPay, @FormParam("totalAmount") Float totalAmount,
+			@FormParam("customerId") int customerId)
+
+	{
+		// pass these values to the register() method, return into a String variable
+		// named output, which can be returned to the client as the response.
+		String output = billObj.addBill(issueDate, unit, balance, amountToPay, totalAmount, customerId);
+		return output;
+	}
+
+	//Update customer details
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateBill(String billData) {
+
+		// Convert the input string to a JSON object
+		JsonObject BillObject = new JsonParser().parse(billData).getAsJsonObject();
+
+		// Read the values from the JSON object
+		String billId = BillObject.get("billId").getAsString();
+		String issueDate = BillObject.get("issueDate").getAsString();
+		String unit = BillObject.get("unit").getAsString();
+		String balance = BillObject.get("balance").getAsString();
+		String amountToPay = BillObject.get("amountToPay").getAsString();
+		String totalAmount = BillObject.get("totalAmount").getAsString();
+		String customerId = BillObject.get("customerId").getAsString();
+
+		String output = billObj.updateBill(issueDate, unit, balance, amountToPay, totalAmount, customerId);
+		return output;
+	}
+
+	//Admin side view all customers 
+	@GET
+	@Path("/viewAllBill")
+	@Produces(MediaType.TEXT_HTML)
+	public String viewAllBill() {
+		return billObj.viewAllBill();
+	}
+
+	//Delete a customer account
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteBill(String billData) {
+
+		// Convert the input string to an XML document
+		Document doc = Jsoup.parse(billData, "", Parser.xmlParser());
+
+		// Read the value from the element <billId>
+		String billId = doc.select("billId").text();
+
+		// Pass this billId can call the deleteBill() method in the model
+		String output = billObj.deleteBill(billId);
+		return output;
+	}
 
 }
--- src/model/Bill.java
+++ src/model/Bill.java
@@ -1,5 +1,202 @@
+//To implement the server-model 
 package model;
 
+import java.sql.*;
+import java.sql.Connection;
+         
 public class Bill {
 
+	// A common method to connect to the DB
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "root");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String addBill(String issueDate, int unit, float balance, float amountToPay , float totalAmount, int customerId ) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			
+			// create a prepared statement
+			String query = " insert into bill(`billId`,`issueDate`,`unit`,`balance`,`amountToPay`, `totalAmount`, `customerId`)"
+					+ " values (?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, issueDate);
+			preparedStmt.setInt(3, unit);
+			preparedStmt.setFloat(4, balance);
+			preparedStmt.setFloat(5, amountToPay);
+			preparedStmt.setFloat(6, totalAmount);
+			preparedStmt.setInt(7, customerId);
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Bill added successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the bill.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	
+	public String viewBill(String billId) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Issue Date</th><th>Unit</th>" + "<th>Balance</th>"
+					+ "<th>Amount To Pay</th>" + "<th>Total Amount</th>"+ "<th>Customer Id</th>"+ "<th>Update</th>";
+
+			String query = "select * from users where userId= '" + billId +"' ";
+			Statement stmt = con.createStatement();
+			ResultSet totalAmount = stmt.executeQuery(query);
+			
+			ResultSet rs = null;
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String billid = Integer.toString(rs.getInt("billId"));
+				String issueDate = rs.getString("issueDate");
+				String unit = Integer.toString(rs.getInt("unit"));
+				String balance = Float.toString(rs.getFloat("balance"));
+				String amountToPay = Float.toString(rs.getFloat("amountToPay"));
+				String totalAmount1 = Float.toString(rs.getFloat("totalAmount"));
+				String customerId = Integer.toString(rs.getInt("customerId"));
+			
+				// Add into the html table
+				output += "<tr><td>" + issueDate + "</td>";
+				output += "<td>" + unit + "</td>";
+				output += "<td>" + balance + "</td>";
+				output += "<td>" + amountToPay + "</td>";
+				output += "<td>" + totalAmount1 +"</td>";
+				output += "<td>" + customerId + "</td>";
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>";
+			
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the bill details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	public String updateBill(String issueDate, String unit, String balance, String amountToPay , String totalAmount, String customerId) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+			// create a prepared statement
+			String query = "UPDATE bill SET issueDate=?,unit=?,balance=?,amountToPay=?,totalAmount=?,customerId=? WHERE billId=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, issueDate);
+			preparedStmt.setString(3, unit);
+			preparedStmt.setString(4, balance);
+			preparedStmt.setString(5, amountToPay);
+			preparedStmt.setString(6, totalAmount);
+			preparedStmt.setString(7, customerId); 
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Bill details updated successfully";
+		} catch (Exception e) {
+			output = "Error while updating the bill details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	public String viewAllBill() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Issue Date</th><th>Unit</th>" + "<th>Balance</th>"
+					+ "<th>Amount To Pay</th>" + "<th>Total Amount</th>"+ "<th>Customer Id</th>"+ "<th>Update</th>";
+
+			String query = "select * from users";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String billid = Integer.toString(rs.getInt("billId"));
+				String issueDate = rs.getString("issueDate");
+				String unit = Integer.toString(rs.getInt("unit"));
+				String balance = Float.toString(rs.getFloat("balance"));
+				String amountToPay = Float.toString(rs.getFloat("amountToPay"));
+				String totalAmount = Float.toString(rs.getFloat("totalAmount"));
+				String customerId = Integer.toString(rs.getInt("customerId"));
+			
+				// Add into the html table
+				output += "<tr><td>" + issueDate + "</td>";
+				output += "<td>" + unit + "</td>";
+				output += "<td>" + balance + "</td>";
+				output += "<td>" + amountToPay + "</td>";
+				output += "<td>" + totalAmount +"</td>";
+				output += "<td>" + customerId + "</td>";
+				// buttons
+				output += "<td><input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the bill details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	public String deleteBill(String billId) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			// create a prepared statement
+			String query = "delete from users where userId=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(billId));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Bill details deleted successfully";
+		} catch (Exception e) {
+			output = "Error while deleting the bill details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 }
